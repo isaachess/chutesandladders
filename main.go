6ec59c7e4283242bb54b73821d6115d2e0e16d7a
Isaac Hess <isaachess@gmail.com>
2021-12-21 20:09:23 -0700

    Make game rolling match

--- main.go
+++ main.go
@@ -4,37 +4,96 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	err := run()
 	if err != nil {
-		log.Fatal("received err", err)
+		log.Fatal(err)
 	}
+}
+
+func bobby() {
+
 }
 
 func run() error {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("How many players are there? ")
 
+	fmt.Print("Choose your goal (1-6): ")
 	text, err := reader.ReadString('\n')
 	if err != nil {
 		return err
 	}
+	goal, err := strconv.Atoi(strings.TrimSpace(text))
+	if err != nil {
+		return err
+	}
 
-	fmt.Println("You answered: ", text)
+	if goal > 6 {
+		return fmt.Errorf("goal must be between 1 and 6")
+	}
+
+	fmt.Print("How many players are there? ")
+
+	text, err = reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
 
 	numPlayers, err := strconv.Atoi(strings.TrimSpace(text))
 	if err != nil {
 		return err
 	}
 
-	names, err := readPlayerNames(numPlayers, reader)
-	fmt.Println("The names of the players are:", names)
+	players, err := readPlayerNames(numPlayers, reader)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("The names of the players are:", players)
+	err = startGame(players, goal)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func startGame(players []string, goal int) error {
+	for {
+		for _, player := range players {
+			won := takeTurn(player, goal)
+			if won {
+				fmt.Printf("Player %s won!\n", player)
+				return nil
+			}
+		}
+	}
+}
+
+func takeTurn(player string, goal int) (won bool) {
+	fmt.Printf("It is %s's turn!\n", player)
+	roll := rollD6()
+	fmt.Printf("%s rolled a %d\n", player, roll)
+	if roll == goal {
+		return true
+	}
+	return false
+}
+
+func rollD6() int {
+	return rollDie(6)
+}
+
+func rollDie(sides int) int {
+	return rand.Intn(sides) + 1
 }
 
 func readPlayerNames(numPlayers int, reader *bufio.Reader) ([]string, error) {
@@ -46,7 +105,7 @@ func readPlayerNames(numPlayers int, reader *bufio.Reader) ([]string, error) {
 		if err != nil {
 			return nil, err
 		}
-		names = append(names, text)
+		names = append(names, strings.TrimSpace(text))
 	}
 	return names, nil
 }
